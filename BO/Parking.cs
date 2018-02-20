ede20918ebec751d40eaa0c898324a2bb15a018c
Unknown <porte.nathan@laposte.net>
2018-02-20 16:23:30 +0100

    Merge branch 'master' into origin/Nathan

--- BO/Parking.cs
+++ BO/Parking.cs
@@ -12,9 +12,7 @@ namespace BO
 
         public String Name { get; set; }
 
-        public Double Longitude { get; set; }
-
-        public Double Latitude { get; set; }
+        public List<Double> Coordinates { get; set; }
 
         public int TotalPlaces { get; set; }
 
