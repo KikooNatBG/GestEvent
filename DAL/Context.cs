dc420911442fd0d719b7e514d4f91c174adc9190
Unknown <porte.nathan@laposte.net>
2018-02-20 14:50:31 +0100

    Init
    Init repo with project

---
+++ DAL/Context.cs
@@ -0,0 +1,19 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Context : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<Theme> Themes { get; set; }
+
+        public DbSet<Parking> Parkings { get; set; }
+    }
+}
