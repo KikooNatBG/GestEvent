e59a5b1a699d810f4e1bde0076a792a46b183422
barbara <barbara.krafft@gmail.com>
2018-02-21 09:31:59 +0100

    try to revert beaucoup migration rename = config problems

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -13,7 +13,5 @@ namespace DAL
         public DbSet<Event> Events { get; set; }
 
         public DbSet<Theme> Themes { get; set; }
-
-        public DbSet<Parking> Parkings { get; set; }
     }
 }
